40bcb180ef0e384ba3e05ae26d58cf36d6f431f8
Tiago <tamarelle15@gmail.com>
2025-11-30 14:08:43 +0100

    Afegits dos jocs de proves (no acabats) de LlistaUsuaris i ProvesUsuari a src

---
+++ src/ProvesLlistaUsuaris.java
@@ -0,0 +1,59 @@
+import dades.excepcions.UsuariDuplicatException;
+import dades.usuaris.*;
+
+public class ProvesLlistaUsuaris {
+    public static void main(String[] args) {
+        
+        LlistaUsuaris usuaris = new LlistaUsuaris();
+
+        Usuari[] llista = {
+        new Pdi("Alies1", "alies1", "Departament 1", "Campus 1"),
+        new Pdi("Alies2", "alies2", "Departament 2", "Campus 2"),
+        new Pdi("Alies3", "alies3", "Departament 3", "Campus 3"),
+
+        new Ptgas("Alies4", "alies4", "Campus 4"),
+        new Ptgas("Alies5", "alies5", "Campus 5"),
+        new Ptgas("Alies6", "alies6", "Campus 6"),
+
+        new Estudiant("Alies7", "alies7", "Ensenyament 7", 2017),
+        new Estudiant("Alies8", "alies8", "Ensenyament 8", 2018),
+        new Estudiant("Alies9", "alies9", "Ensenyament 9", 2019),
+        };
+
+        System.out.println("Inserció amb àlies ordenats i intent de afegir duplicat");
+        for (int i = 0; i < 9; i++){
+            try {
+                usuaris.afegir(llista[i]);
+            } catch (UsuariDuplicatException e){
+                System.out.println(e);
+            }
+        }
+
+        try {
+                usuaris.afegir(llista[0]);
+            } catch (UsuariDuplicatException e){
+                System.out.println(e);
+            }
+
+        System.out.println(usuaris);
+        
+        LlistaUsuaris usuaris2 = new LlistaUsuaris();
+        System.out.println("Inserció amb àlies desordenats");
+        for (int i = 0; i < 9; i++){
+            try {
+                usuaris2.afegir(llista[9-i-1]);
+            } catch (UsuariDuplicatException e){
+                System.out.println(e);
+            }
+        }
+
+        try {
+                usuaris2.afegir(llista[0]);
+            } catch (UsuariDuplicatException e){
+                System.out.println(e);
+            }
+
+        System.out.println(usuaris2);
+
+    }
+}
---
+++ src/ProvesUsuari.java
@@ -0,0 +1,22 @@
+import dades.usuaris.*;
+public class ProvesUsuari {
+    public static void main(String[] args) {
+        Usuari[] llista = {
+        new Pdi("Alies1", "alies1", "Departament 1", "Campus 1"),
+        new Pdi("Alies2", "alies2", "Departament 2", "Campus 2"),
+        new Pdi("Alies3", "alies3", "Departament 3", "Campus 3"),
+
+        new Ptgas("Alies4", "alies4", "Campus 4"),
+        new Ptgas("Alies5", "alies5", "Campus 5"),
+        new Ptgas("Alies6", "alies6", "Campus 6"),
+
+        new Estudiant("Alies7", "alies7", "Ensenyament 7", 2017),
+        new Estudiant("Alies8", "alies8", "Ensenyament 8", 2018),
+        new Estudiant("Alies9", "alies9", "Ensenyament 9", 2019),
+        };
+        
+        for (int i = 0; i < 9; i++){
+            System.out.println(llista[i]);
+        }
+    }
+}
