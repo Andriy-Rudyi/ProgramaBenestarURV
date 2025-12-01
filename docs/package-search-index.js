9acf1880f272c8549a91413be7e882100a76a212
Andriy Rudyi <andriyrudyy22@gmail.com>
2025-12-01 12:34:10 +0100

    docs: add/update documentation

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"dades"},{"l":"dades.activitats"},{"l":"dades.excepcions"},{"l":"dades.inscripcions"},{"l":"dades.usuaris"}]
