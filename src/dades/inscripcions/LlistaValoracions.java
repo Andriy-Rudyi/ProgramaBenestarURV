75971384025da0428a5780156fb706c56aa7ccc9
ImTaigu <tamarelle15@gmail.com>
2025-12-18 17:17:38 +0100

    refactor: modified unfinished afegirValoracio and added getMitjanaValoracions();

--- src/dades/inscripcions/LlistaValoracions.java
+++ src/dades/inscripcions/LlistaValoracions.java
@@ -4,18 +4,28 @@ import dades.excepcions.UsuariDuplicatException;
 import dades.usuaris.Usuari;
 
 public class LlistaValoracions {
-    Usuari[] llistaUsuaris;
+    Usuari[] llistaUsuaris;     /*no usem LlistaUsuaris perquè aquí no volem que les ordeni per alies
+                                ja que si no, els índex de LlistaUsuaris no pertanyeran als de int[] */ 
     int[] valoracions;
     int numValoracions;
-    private static final int MIDA_INICIAL = 50;
 
-    public LlistaValoracions(){
-        llistaUsuaris = new Usuari[MIDA_INICIAL];
-        valoracions = new int[MIDA_INICIAL];
+    public LlistaValoracions(int numMaximInscripcions){
+        llistaUsuaris = new Usuari[numMaximInscripcions];
+        valoracions = new int[numMaximInscripcions];
         numValoracions = 0;
     }
 
-    public void afegirValoracio(Usuari usuari, double valoracio){
+    public int getNumValoracions(){ return numValoracions; }
+
+    public double getMitjanaValoracions(){
+        int sumaValoracions = 0;
+        for (int i = 0; i < numValoracions; i++){
+            sumaValoracions = sumaValoracions + valoracions[i];
+        }
+        return ( (double) sumaValoracions ) / numValoracions ;
+    }
+
+    public void afegirValoracio(Usuari usuari, int valoracio) throws UsuariDuplicatException{
             // null check
         if (usuari == null) {
             return;
@@ -23,33 +33,12 @@ public class LlistaValoracions {
         
         // Comprovar si ja existeix
         for(int i = 0; i < numValoracions; i++){
-            if (usuari.equals(llistaUsuaris[i])) return; //TODO què fem aquí?
+            if (usuari.equals(llistaUsuaris[i])) throw new UsuariDuplicatException("Aquest usuari ja ha fet una valoració");
         }
         
-        // Redimensionar si cal
-        if (numUsuaris == llista.length) {
-            private void redimensionar() {
-            Usuari[] novaLlista = new Usuari[llista.length * 2];
-            for (int i = 0; i < numUsuaris; i++) {
-                novaLlista[i] = llista[i];
-            }
-        llista = novaLlista;
+        // Inserir (sense cap ordre)
+        llistaUsuaris[numValoracions] = usuari;
+        valoracions[numValoracions] = valoracio;
     }
-        }
-        
-        // Trobar posició d'inserció mantenint ordre alfabètic
-        int pos = 0;
-        while (pos < numUsuaris && llista[pos].getAlies().compareTo(usuari.getAlies()) < 0) {
-            pos++;
-        }
-        
-        // Desplaçar elements cap a la dreta
-        for (int i = numUsuaris; i > pos; i--) {
-            llista[i] = llista[i - 1];
-        }
-        
-        // Inserir l'usuari
-        llista[pos] = usuari.copia();
-        numUsuaris++;
-    }
+
 }
